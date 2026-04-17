6c9f0cf226fa5710c3b5ee36e9851f5a7267a0c9
battryleaf-dev <battryleaf@gmail.com>
2026-04-17 15:47:23 +0400

    Delete 3x3-hotbar-under-deve-hopefully-main directory

--- 3x3-hotbar-under-deve-hopefully-main/README.txt
+++
@@ -1,2 +0,0 @@
-honestly idk what im doing im just following chatgpt
-wilted rose emoji
--- 3x3-hotbar-under-deve-hopefully-main/src/main/java/com/example/hotbargrid/HotbarGridState.java
+++
@@ -1,6 +0,0 @@
-package com.example.hotbargrid;
-
-public class HotbarGridState {
-    // no longer needed for positioning but kept in case other code references it
-    public static int GRID_BOTTOM_Y = 0;
-}
